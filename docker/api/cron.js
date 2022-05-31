a0f22247bcc818eb4206741c5097e8ad549e45b4
EasyChen <easychen@qq.com>
2022-05-31 14:20:53 +0800

    修改显示文案

---
+++ docker/api/cron.js
@@ -0,0 +1,208 @@
+const fs = require("fs");
+const dayjs = require("dayjs");
+const { monitor_auto, send_notify, get_data_dir, cron_check, logstart, logit, to_markdown, short } = require("./func");
+
+const data_file = get_data_dir() + 'data.json';
+const content = fs.readFileSync( data_file );
+const json_data = JSON.parse( content );
+
+const to_checks = json_data.checks.filter( item => parseInt(item.enabled) === 1 && parseInt(item.is_cloud_task||0) >= 1 );
+
+run = async () =>{
+logstart();
+logit("开始载入任务");
+for( const item of to_checks )
+{
+    let do_now = false;
+    if( parseInt( item.interval ) < 0 ) item.interval = 0;
+
+    // 判断是否应该监测
+    if( item.last_time )
+    {
+        if( item.interval > 0 )
+        {
+            if(dayjs(item.last_time).add(item.interval,'minutes').isBefore(dayjs())) do_now = true; 
+        }
+
+        if( item.interval == 0 )
+        {
+            do_now = true; 
+        }
+    }else
+    {
+        do_now = true; 
+    }
+
+    // 处理cron逻辑
+    if( item.cron && !cron_check(item.cron) )
+    {
+        do_now = false;
+        console.log("监测时间监测，跳过"+item.cron);
+    }
+    
+    // 处理retry
+    if( parseInt(item.retry) < 1 ) item.retry = 10;
+    if( parseInt(item.retry_times) > parseInt(item.retry) )
+    {
+        do_now = false; 
+        logit("重试次数超过，跳过"+item.title);
+    }
+    logit( item.title + "...条件检查 " +do_now );
+    if( do_now )
+    {
+        // 返回状态，默认为成功
+        // 0:未检测，1:成功但没有变动，2：成功且有异动
+        let check_status = 0; 
+        let check_content = ""; 
+        let check_html = ""; 
+        let check_link = ""; 
+        
+        logit("checking..."+item.title, dayjs().format('YYYY-MM-DD HH:mm:ss'));
+        
+        const ret = await monitor_auto( item, json_data.cookies );
+        if( ret && ret.status )
+        {
+            check_content = ret.value;
+            if( ret.html ) check_html = ret.html;
+            if( ret.link ) check_link = ret.link;
+            check_status = 1;
+            
+        }
+        else
+            check_status = -1;
+
+        const {html, ...ret_short} = ret;
+        logit( ret_short );
+
+        if( check_status < 0 )
+        {
+            // 失败
+            // 重试流程
+            const retry_times = parseInt( item.retry_times||0 );
+            if( retry_times >= item.retry )
+            {
+                // 发送通知
+                await send_notify( '监测点['+item.title+']多次重试失败', "已暂停执行，请检查登录状态或页面结构变动\r\n\r\n[点此查看]("+item.url+")" , item.sendkey, item.send_channel||-1);
+            }
+            check_update_field( item.id, 'retry_times', retry_times+1, json_data );
+            
+        }else
+        {
+            // 成功分支
+            const last_content = item.last_content;
+            
+            // 先更新再发通知，避免发送操作中断
+            check_update_field( item.id, 'last_content', check_content, json_data );
+            check_update_field( item.id, 'last_time', Date.now(), json_data );
+            
+            // 重试计数清理
+            check_update_field( item.id, 'retry_times', 0, json_data);
+            
+            let can_send_notice = true;
+
+            if( item.when == 'change' )
+            {
+                logit("变动时发送");
+                if( !last_content || (check_content?.trim() == last_content?.trim()) )
+                {
+                    logit("内容相同或者旧内容不存在，跳过");
+                    can_send_notice = false;
+                }else
+                {
+                    console.log( last_content,check_content );
+                }
+            }
+
+            if( item.compare_type == 'regex' )
+            {
+                logit("正则匹配模式");
+                if( item.regex && !check_content.match( new RegExp(item.regex, 'i') ) )
+                {
+                    can_send_notice = false;
+                    logit( '通知正则不匹配，不发送通知' );
+                }
+            }
+            
+            if( item.compare_type == 'op' )
+            {
+                logit("条件比较模式");
+                
+                let the_value = item.compare_value;
+                            
+                if( item.compare_value == '*请求返回状态码*' ) the_value = item.code;
+                
+                if( item.compare_value == '*上次监测返回值*' ) the_value = last_content;
+                
+
+                if( item.compare_op == 'ne' && !(check_content != the_value)) can_send_notice = false;
+
+                if( item.compare_op == 'eq' && !(check_content == the_value)) can_send_notice = false;
+
+                if( item.compare_op == 'gt' && !(parseFloat(check_content) > parseFloat(the_value||0))) can_send_notice = false;
+
+                if( item.compare_op == 'gte' && !(parseFloat(check_content) >= parseFloat(the_value||0))) can_send_notice = false;
+
+                if( item.compare_op == 'lt' && !(parseFloat(check_content) < parseFloat(the_value||0))) can_send_notice = false;
+
+                if( item.compare_op == 'lte' && !(parseFloat(check_content) <= parseFloat(the_value||0))) can_send_notice = false;
+
+                // console.log("op:",check_content,item.compare_op,the_value,can_send_notice);
+
+                logit("op:"+parseFloat(check_content)+' '+item.compare_op+' '+parseFloat(the_value||0)+' '+can_send_notice);
+
+            } 
+                
+              
+            
+            if( can_send_notice )
+            {
+                logit( '已发送通知' );
+            
+                if( item.sendkey )
+                {
+                    const title = '监测点['+item.title+']有新通知';
+                                
+                    let desp = short(check_content,50) + (last_content && item.when == 'change' ? ('←' + short(last_content,50)):"");
+
+                    const link = check_link || item.page || item.url;
+                    const short_content = desp;
+
+                    desp += "\r\n\r\n[详情链接]("+ link +") \r\n\r\n";
+                    
+                    if( check_html )
+                        desp += "\r\n\r\n---\r\n\r\n" + to_markdown(check_html); 
+                    else
+                    {
+                        if( String(check_content).length > 50 )
+                        {
+                            desp += "\r\n\r\n---\r\n\r\n" + check_content; 
+                        }
+                    }
+                    
+                    // const title = check_content.length > 50 ? '监测点['+item.title+']有新通知' : ( '监测点['+item.title+']有新通知: ' + check_content + (item.last_content ? ('←' + item.last_content):"") );
+
+                    // const desp = check_content.length > 50 ? (check_html ? to_markdown(check_html) : check_content) :  check_content + (item.last_content ? ('←' + item.last_content):"") ;
+                    
+                    
+                    await send_notify( title, desp , item.sendkey, item.send_channel || -1, short_content);  
+                }
+            }
+        } 
+    }
+}
+
+logit("全部任务处理完成");
+
+} 
+
+run();
+
+
+
+async function check_update_field( id, field, value, json_data )
+{
+    const the_idx = json_data.checks.findIndex(item => item.id == id);
+    if( the_idx < 0 ) return false;
+    json_data.checks[the_idx][field] = value;
+    fs.writeFileSync( data_file, JSON.stringify(json_data) );
+}
---
+++ docker/api/func.js
@@ -0,0 +1,473 @@
+const rssParser = require('rss-parser');
+const jsonQuery = require('json-query');
+const timeoutSignal = require("timeout-signal");
+const fetch = require('cross-fetch');
+const FormData = require('form-data');
+const turndown = require('turndown');
+const puppeteer = require('puppeteer');
+const path = require("path");
+const fs = require("fs");
+const dayjs = require("dayjs");
+const { JSDOM } = require("jsdom");
+
+get_data_dir = ()=>
+{
+    return parseInt(process.env.DEV) > 0 ? path.join( __dirname, '/../data/') : '/data/';
+}
+
+const log_file = get_data_dir() + 'log.txt';
+
+exports.get_data_dir = get_data_dir;
+
+exports.logstart = () =>
+{
+    if( fs.existsSync( log_file ) )
+    {
+        fs.unlinkSync( log_file );
+    }
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+exports.short = short;
+
+function short( maybe_string , len = 0 )
+{
+    if( !maybe_string ) return false;
+    if( isNumeric( maybe_string )) return maybe_string;
+    if (typeof maybe_string === 'string' || maybe_string instanceof String)
+    {
+        if( len < 1 ) return maybe_string.replace(/^"(.+)"$/ig, '\$1');
+        return maybe_string.replace(/^"(.+)"$/ig, '\$1').substring( 0, len );
+    }else
+    {
+        // maybe object
+        if( len < 1 ) return JSON.stringify(maybe_string);
+        return JSON.stringify(maybe_string).substring(0,len);
+    }
+}
+
+exports.to_markdown = ( html ) => 
+{
+    const c = new turndown();
+    return c.turndown(html);
+}
+
+logit = ( text ) =>
+{
+    console.log( text );
+    if (typeof text !== 'string' && !(text instanceof String))
+    {
+        text = JSON.stringify( text );
+    }
+    fs.appendFileSync( log_file, '['+ dayjs().format('YYYY-MM-DD HH:mm:ss') + ']' +text + "\r\n" );
+}
+
+exports.logit = logit;
+
+function range(start, stop, step) {
+    var a = [start], b = start;
+    while (b < stop) {
+        a.push(b += step || 1);
+    }
+    return a;
+}
+
+exports.cron_check = ( cron, now = null )=>
+{
+    const dinfo = dayjs(now||Date.now()).format('m-H-D-M-d').split('-');
+    const cinfo = cron.trim().split(' ');
+    // console.log( dinfo, cinfo );
+    let ret = true;
+    for( let i=0; i < 5; i++ )
+    {
+        let cline = cinfo[i].trim();
+        let dline = dinfo[i].trim();
+        if( cline != "*" )
+        {
+            if( cline.indexOf(',') >= 0 )
+            {
+                const cpart = cline.split(',');
+                if( cline.indexOf('-') >= 0 )
+                {
+                    // citem 全部用int，其他地方用string
+                    let citem = [];
+                    for( let cp of cpart )
+                    {
+                        // console.log(cp);
+                        if( cp.indexOf('-') >= 0 )
+                        {
+                            const cpinfo = cp.split('-');
+                            // console.log( cpinfo );
+                            cp = range(  parseInt(cpinfo[0]),parseInt(cpinfo[1]),1 );
+                            citem = citem.concat( cp );
+                        }else
+                        {
+                            citem.push( parseInt(cp) );
+                        }
+                    }
+                    if( !citem.includes(parseInt(dline)) ) ret = false;
+                    console.log( citem );
+                    
+                }else
+                {
+                    if( !cpart.includes(dline) ) ret = false;
+                }
+                
+            }else
+            {
+                if( cline != dline ) ret = false;
+            }
+        }
+
+        console.log( cline, dline, ret );
+        
+    }
+    return ret;
+}
+
+exports.to_time_string = ( date ) =>
+{
+    return dayjs( date ).format('YYYY-MM-DD HH:mm:ss');
+}
+
+exports.get_cookies = () =>
+{
+    const data_file = get_data_dir() + 'data.json';
+    const content = fs.readFileSync( data_file );
+    const json_data = JSON.parse( content );
+    return json_data.cookies;
+}
+
+exports.send_notify = async ( title, desp, sendkey, channel = -1, short = false)  =>
+{
+    try {
+        const form = new FormData();
+        if( channel >= 0 ) form.append( 'channel',parseInt(channel));
+        if( short ) form.append( 'short',short ); 
+        form.append( 'title',title ); 
+        form.append( 'desp',desp.substring(0,10000) ); 
+        const response = await fetch( 'https://sctapi.ftqq.com/'+sendkey+'.send', {
+            method: 'POST', 
+            body: form
+        }  );
+
+        const data = await response.text();
+        return JSON.parse(data)||data;
+    } catch (error) {
+        console.log("推送微信通知错误",error);
+        return false;
+    }
+}
+
+exports.monitor_auto = async ( item, cookies ) =>
+{
+    const domain = new URL( item.url ).host;
+    const the_cookies = cookies[domain];
+    console.log("in auto");
+    let ret;
+    try {
+        switch (item.type) {
+            case 'get':
+                ret = await monitor_get( item.url, (parseInt(item.delay)||0)*1000 );
+                return {status:!!ret,value:ret||0,type:item.type};
+                break;
+            case 'rss':
+                ret = await monitor_rss( item.url, (parseInt(item.delay)||0)*1000 );
+                return {status:!!(ret&&ret[item.rss_field]),value:ret[item.rss_field]||"",link:ret.link,type:item.type};
+                break;
+            case 'json':
+                // 特别注意，云端的json监测包含delay参数
+                // console.log(item);
+                ret = await monitor_json( item.url, item.json_query, item.json_header, item.json_data, item.json_data_format, (parseInt(item.delay)||0)*1000, the_cookies );
+                return {status:!!(ret&&ret.value),value:JSON.stringify(ret.value)||"",type:item.type};
+                break;
+            case 'dom':
+            default:
+                ret = await monitor_dom( item.url, item.path, (parseInt(item.delay)||0)*1000, the_cookies, item.id );
+                return {status:!!(ret&&ret.text),value:ret.text||"",type:item.type,html:ret.html||"",link:ret.link||""};
+        }
+    } catch (error) {
+        console.log(error);
+        return {status:false,error,type:item.type};
+    }
+    
+}
+
+async function monitor_get(url,timeout=10000)
+{
+    const response = await fetch( url, { signal: timeoutSignal(timeout<1?10000:timeout) } );
+    return response.status;
+}
+
+async function monitor_json(url, query, header=false, body_string=false, format = 'form', timeout=10000, cookies=[])
+{
+    try {
+        const body_data = body_string? JSON.parse(body_string||"{}") : false;
+        let body = body_data ? JSON.stringify( body_data ): false;
+        console.log( body_data, body );
+
+        if( format == 'form' )
+        {
+            const params = new FormData();
+            if( body_data )
+                Object.keys( body_data ).forEach( item => params.append(item, body_data[item]) );
+            body = params;
+        }
+
+        let headers = header ? (JSON.parse(header) || false) : false;
+        const cookie_string = build_cookie_string(cookies);
+        if( cookie_string )
+        {
+            if( !headers ) headers = {};
+            headers['cookie'] = cookie_string;
+        }
+        const method = body_string ? "POST" : "GET";
+        let opt = {
+            method,
+            credentials: 'include',
+            signal: timeoutSignal(timeout<1?10000:timeout), 
+        } ;
+
+        if( headers ) opt.headers = headers;
+        if( method == 'POST' ) opt.body = body;
+
+        const response = await fetch( url, opt );
+        const data = await response.json();
+        const ret = jsonQuery( query ,{data} );
+        console.log( ret );
+        if( !( ret && ret.value ) )
+        {
+            console.log("save error");
+            const image_dir = get_data_dir()+'/image';
+                
+            if( !fs.existsSync(image_dir) )
+            fs.mkdirSync(image_dir);
+            
+            fs.writeFileSync( image_dir + '/error.json', JSON.stringify(data) );
+        }
+        return ret; 
+    } catch (error) {
+        console.log( error );
+        return false;
+    }
+    
+}
+
+// rssParser
+async function monitor_rss(url,timeout=10000)
+{
+    const parser = new rssParser({ timeout });
+    const site = await parser.parseURL( url );
+    const ret = site.items[0]||false;
+    return ret;
+}
+
+async function monitor_dom_low(url, path, delay , cookies)
+{
+    console.log("in low dom");
+    try {
+        const response = await fetch( url, { signal: timeoutSignal(delay<1?10000:delay) } );
+        const all = await response.text();
+        if( all.substring(0,2000).toLowerCase().indexOf('utf-8') < 0 ) return  false;
+        // const sniffedEncoding = htmlEncodingSniffer(await response.arrayBuffer());
+        // console.log(sniffedEncoding);
+
+        const dom = new JSDOM(all);
+        const ret = dom.window.document.querySelectorAll(path);
+
+        let texts = [];
+        let html = "";
+        for( let item of ret )
+        {
+            item.querySelectorAll("[src]").forEach( item => { if( item.src.substr(0,4) != 'http' ) { item.src = new URL(url).origin +( item.src.substr(0,1) == '/' ? item.src : '/'+ item.src  )   } } );
+            
+            if( item.textContent ) texts.push(item.textContent?.trim());
+            html += item.outerHTML ? item.outerHTML + "<br/>" : ""; 
+        }
+
+        return {text:path.indexOf(",") >= 0 ? texts.join("\n") :texts[0]||"",html,all};
+    } catch (error) {
+        console.log("low dom error",error);
+        return false;
+    }
+}
+
+async function monitor_dom(url, path, delay , cookies, id)
+{
+    console.log("in dom delay = ",delay);
+    if((!process.env.SNAP_URL_BASE) && delay < 1 )
+    {
+        const first = await monitor_dom_low(url, path, delay , cookies);
+        console.log( "low result" , first.text );
+        if( first && first.text ) return first;
+    }else
+    {
+        console.log( "jump jsdom" );
+    } 
+    
+    let opt = {
+        args: ['--no-sandbox'],
+        defaultViewport: null,
+        headless: true, 
+        timeout:delay+1000*10,
+    };
+
+    if( process.env.CHROMIUM_PATH ) 
+        opt['executablePath'] = process.env.CHROMIUM_PATH;
+
+    const browser = await puppeteer.launch(opt);
+
+    console.log(delay);
+    let ret = false;
+    
+    const page = await browser.newPage(); 
+    await page.setDefaultNavigationTimeout(delay+1000*10);
+    // await page.setDefaultNavigationTimeout(0);
+    if( isIterable(cookies) )
+        await page.setCookie( ...cookies ); 
+
+    await page.evaluateOnNewDocument(() => { HTMLVideoElement.prototype.canPlayType = function () { return 'probably' }; });   
+    
+    try {
+        
+        await page.goto(url,{
+            waitUntil: 'networkidle2',
+            timeout: delay+1000*10
+        });
+
+        if( delay > 0 )
+        {
+            await sleep(delay);
+        }
+
+        ret = await page.evaluate( (path) => {
+            let ret = window.document.querySelectorAll(path);
+            if( !ret ) return false;
+            console.log("query fail",path,ret);
+            let texts = [];
+            let html = "";
+            for( let item of ret )
+            {
+                item.querySelectorAll("[src]").forEach( item => { if( item.src.substr(0,4) != 'http' ) { item.src = window.origin +( item.src.substr(0,1) == '/' ? item.src : '/'+ item.src  )   } } );
+                
+                if( item.innerText ) texts.push(item.innerText?.trim());
+                html += item.outerHTML ? item.outerHTML + "<br/>" : ""; 
+            }
+            return {html,text:path.indexOf(",") >= 0 ? texts.join("\n") :texts[0]||"","all":window.document.documentElement.innerHTML};
+        },path);
+        
+        if( !ret )
+        {
+            console.log("sleep",1000*5);
+            await sleep(1000*5);
+            ret = await page.evaluate( (path) => {
+                let ret = window.document.querySelectorAll(path);
+                console.log("query fail again",path,ret);
+                if( !ret ) return false;
+                let texts = [];
+                let html = "";
+                for( let item of ret )
+                {
+                    item.querySelectorAll("[src]").forEach( item => { if( item.src.substr(0,4) != 'http' ) { item.src = window.origin +( item.src.substr(0,1) == '/' ? item.src : '/'+ item.src  )   } } );
+                    
+                    if( item.innerText ) texts.push(item.innerText?.trim());
+                    html += item.outerHTML ? item.outerHTML + "<br/>" : ""; 
+                }
+                return {html,text:path.indexOf(",") >= 0 ? texts.join("\n") :texts[0]||"","all":window.document.documentElement.innerHTML};
+            },path);
+            
+        }
+        const { all,html, ...ret_short } = ret;
+        
+        
+        
+        console.log("ret",ret_short);
+        
+        const image_dir = get_data_dir()+'/image';
+        
+        if(ret.all)
+        {
+            console.log("save html");
+            fs.writeFileSync( image_dir +'/'+ id +'.html', ret.all );
+        }
+            
+            
+        // 如果返回值为空，那么保存错误现场
+        if( !(ret && ret.text) )
+        {
+            if( !fs.existsSync(image_dir) )
+            fs.mkdirSync(image_dir);
+            
+            // 写入html
+            if(ret.all)
+                fs.writeFileSync( image_dir + '/error.html', ret.all );
+
+            if( process.env.ERROR_IMAGE && process.env.ERROR_IMAGE != 'NONE' )
+            {
+                console.log("生成截图 http://.../image/error.jpg");
+
+                await page.setViewport({ width:1300, height:1000 });
+
+                await page.screenshot({"path":image_dir+'/error.jpg',"type":"jpeg","captureBeyondViewport":false,"fullPage":process.env.ERROR_IMAGE=='FULL'||false});
+            }
+           
+        }else
+        {
+            // 如果启用了返回截图功能
+            if( process.env.SNAP_URL_BASE )
+            {
+                if( !fs.existsSync(image_dir) )
+                fs.mkdirSync(image_dir);
+
+                const image_path = `${image_dir}/${id}.jpg`;
+
+                const mobile = puppeteer.devices['iPhone X']
+                await page.emulate(mobile);
+                await page.setViewport({ width:480, height:2000 });
+                
+                await page.screenshot({"path":image_path,"type":"jpeg","captureBeyondViewport":false,"fullPage":process.env.SNAP_FULL||false});
+
+                if( fs.existsSync(image_path) )
+                {
+                    console.log("截图完成");
+                    ret.link = process.env.SNAP_URL_BASE + '/image/' + id +'.jpg?key='+process.env.API_KEY;
+                } 
+            }
+        }  
+    } catch (error) {
+        console.log("error",error);
+        
+    }finally
+    {
+        await browser.close();
+        return ret;
+    }
+}
+
+function isIterable(obj) {
+    // checks for null and undefined
+    if (obj == null) {
+      return false;
+    }
+    return typeof obj[Symbol.iterator] === 'function';
+}
+
+function sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
+
+function build_cookie_string( cookie_array )
+{
+    let ret = [];
+    for(  const cookie of cookie_array )
+    {
+        if( cookie.name ) ret.push(`${cookie.name}=${cookie.value}`)
+    }
+    return ret.length > 0 ? ret.join('; ') : false;
+}
+
